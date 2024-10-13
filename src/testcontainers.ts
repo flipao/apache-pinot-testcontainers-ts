a71db0ff9643835d283081e9045abc475c3ecc42
Felipe da Cunha Meirelles <fcmeirelles@gmail.com>
2024-10-13 20:26:16 +0200

    chore: clean up

--- src/testcontainers.ts
+++ src/testcontainers.ts
@@ -1,12 +1,3 @@
-import {
-  TestContainer,
-  // StartedTestContainer,
-  // StoppedTestContainer,
-  GenericContainer,
-} from 'testcontainers';
+import { TestContainer, GenericContainer } from 'testcontainers';
 
 export const container: TestContainer = new GenericContainer('alpine');
-
-// export const startedContainer: StartedTestContainer = await container.start();
-// export const stoppedContainer: StoppedTestContainer =
-//   await startedContainer.stop();
